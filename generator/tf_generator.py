26a460111e1b85a80d8337fabc6ab4c806cb3990
riese <github-riese@iotaomegapsi.de>
2022-05-26 00:59:45 +0200

    new model
    extra conv2d + maxpool, less params

--- generator/tf_generator.py
+++ generator/tf_generator.py
@@ -1,6 +1,7 @@
 from keras.applications.vgg16 import VGG16
 from keras.initializers.initializers_v2 import RandomNormal
-from keras.layers import LeakyReLU, Conv2D, Conv2DTranspose, Normalization, BatchNormalization, UpSampling2D
+from keras.layers import LeakyReLU, Conv2D, Conv2DTranspose, Normalization, BatchNormalization, UpSampling2D, MaxPool2D, \
+    ReLU
 from keras.models import Sequential
 
 
@@ -10,13 +11,13 @@ class ImageGenerator:
         self._features.trainable = False
 
         self._generate = Sequential([
-            Conv2DTranspose(256, 10, 5, activation=LeakyReLU(), padding='same', kernel_initializer=RandomNormal(),
+            Conv2DTranspose(256, 4, 4, activation=LeakyReLU(), padding='same', kernel_initializer=RandomNormal(),
                             use_bias=False),
-            BatchNormalization(),
-            Conv2DTranspose(256, 4, 4, activation=LeakyReLU(), kernel_initializer=RandomNormal(), use_bias=False),
-            Conv2DTranspose(128, 2, 2, activation=LeakyReLU(), kernel_initializer=RandomNormal(), use_bias=False),
-            Conv2DTranspose(128, 1, 1, activation=LeakyReLU(), use_bias=False, kernel_initializer=RandomNormal()),
-            BatchNormalization(),
+            Conv2D(128, 4, 4, use_bias=False, activation=ReLU()),
+            MaxPool2D(),
+            Conv2DTranspose(256, 5, 5, activation=None, kernel_initializer=RandomNormal(), use_bias=False),
+            Conv2DTranspose(128, 4, 4, activation=None, kernel_initializer=RandomNormal(), use_bias=False),
+            Conv2DTranspose(128, 4, 4, activation=LeakyReLU(), kernel_initializer=RandomNormal(), use_bias=False),
             Conv2D(3, 1, 1, use_bias=False, activation='sigmoid', kernel_initializer=RandomNormal())
         ])
 
