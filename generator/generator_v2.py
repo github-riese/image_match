246dc7a0f65fc6886ab17d38e102a4e489a75739
riese <github-riese@iotaomegapsi.de>
2022-09-02 21:31:27 +0200

    reduced img sizes to 32x32

--- generator/generator_v2.py
+++ generator/generator_v2.py
@@ -2,9 +2,9 @@ import numpy as np
 import tensorflow as tf
 from keras import backend as K, regularizers
 from keras.layers import Dense, Flatten, Reshape, Conv2DTranspose, \
-    Lambda, Normalization, Dropout, GaussianNoise, MaxPooling2D
+    Lambda, Dropout, GaussianNoise, MaxPooling2D
 from keras.legacy_tf_layers.convolutional import Conv2D
-from keras.optimizer_v2.nadam import Nadam
+from keras.optimizers import Nadam
 
 
 class NoisyNadam(Nadam):
@@ -23,7 +23,7 @@ class NoisyNadam(Nadam):
             grads_and_vars = [
                 (
                     tf.add(gradient,
-                           tf.random.normal(stddev=stddev * 1.01 ** (layers - n), mean=0., shape=gradient.shape)),
+                           tf.random.truncated_normal(stddev=stddev * 1.01 ** (layers - n), mean=0., shape=gradient.shape)),
                     var) for n, (gradient, var) in enumerate(grads_and_vars)]
         return super().apply_gradients(grads_and_vars, name, experimental_aggregate_gradients)
 
@@ -35,45 +35,46 @@ class Generator(tf.keras.Model):
         self._mean = None
         self._log_var = None
 
-        self._noise = GaussianNoise(.5)
-        self._norm = Normalization()
+        self._noise_1 = GaussianNoise(1.)
+        self._noise_2 = GaussianNoise(.9)
+        self._noise_3 = GaussianNoise(.81)
+        self._noise_4 = GaussianNoise(.72)
 
-        self._conv_1 = Conv2D(64, 3, activation='leaky_relu', padding='same')
-        self._conv_2 = Conv2D(64, 3, activation='leaky_relu', padding='same')
-        self._pool_1 = MaxPooling2D((2, 2), (2, 2))
+        self._conv_1 = Conv2D(64, 3, activation='leaky_relu', padding='same', name='conv2d_1')
+        self._pool_1 = MaxPooling2D(2, 2)
 
-        self._conv_3 = Conv2D(128, 3, activation='leaky_relu', padding='same')
-        self._conv_4 = Conv2D(128, 3, activation='leaky_relu', padding='same')
+        self._conv_3 = Conv2D(128, 3, activation='leaky_relu', padding='same', name='conv2d_3')
         self._pool_2 = MaxPooling2D(2, 2)
 
-        self._conv_5 = Conv2D(256, 3, activation='leaky_relu', padding='same')
-        self._conv_6 = Conv2D(256, 3, activation='leaky_relu', padding='same')
+        self._conv_5 = Conv2D(256, 3, activation='leaky_relu', padding='same', name='conv2d_5')
         self._pool_3 = MaxPooling2D(2, 2)
 
-        self._conv_7 = Conv2D(512, 3, activation='leaky_relu', padding='same')
-        self._conv_8 = Conv2D(512, 3, activation='leaky_relu', padding='same')
+        self._conv_7 = Conv2D(512, 3, activation='leaky_relu', padding='same', name='conv2d_7')
         self._pool_4 = MaxPooling2D(2, 2)
 
         self._flatten = Flatten()
 
         self._dropout = Dropout(rate=0.5)
         self._latent = Lambda(self._compute_latent, output_shape=(latent_size,))
-        self._latent_mean = Dense(latent_size, activity_regularizer=regularizers.l1(0.05))
-        self._latent_log_var = Dense(latent_size, activity_regularizer=regularizers.l1(0.05))
+        self._latent_mean = Dense(latent_size)
+        self._latent_log_var = Dense(latent_size)
 
-        self._dense1 = Dense(latent_size, activation='leaky_relu', activity_regularizer=regularizers.l1(0.025))
+        self._dense1 = Dense(latent_size, activation='leaky_relu', activity_regularizer=regularizers.l1(0.04))
 
         self._reshape = Reshape(target_shape=(1, 1, latent_size))
 
         self._generate_1 = Conv2DTranspose(512, 2, 2, use_bias=True, activation='leaky_relu',
-                                           activity_regularizer=regularizers.l2(0.02))
+                                           kernel_regularizer=regularizers.l2(.02))
         self._generate_2 = Conv2DTranspose(256, 2, 2, use_bias=True, activation='leaky_relu')
-        self._generate_3 = Conv2DTranspose(128, 5, 5, use_bias=True, activation='leaky_relu')
+        self._generate_3 = Conv2DTranspose(128, 2, 2, use_bias=True, activation='leaky_relu',
+                                           kernel_regularizer=regularizers.l2(.015))
         self._generate_4 = Conv2DTranspose(96, 2, 1, use_bias=True, activation='leaky_relu', padding='same')
-        self._generate_5 = Conv2DTranspose(72, 2, 2, use_bias=True, activation='leaky_relu')
-        self._generate_6 = Conv2DTranspose(64, 2, 1, use_bias=True, activation='leaky_relu', padding='same')
-        self._generate_7 = Conv2DTranspose(48, 2, 2, use_bias=True, activation='leaky_relu')
-        self._generate_8 = Conv2DTranspose(48, 2, 1, use_bias=True, activation='leaky_relu', padding='same')
+        self._generate_5 = Conv2DTranspose(72, 2, 2, use_bias=True, activation='leaky_relu',
+                                           kernel_regularizer=regularizers.l2())
+        self._generate_6 = Conv2DTranspose(64, 2, 1, use_bias=True, activation='leaky_relu', padding='same',
+                                           kernel_regularizer=regularizers.l2())
+        self._generate_7 = Conv2DTranspose(48, 2, 2, use_bias=True, activation='leaky_relu',
+                                           kernel_regularizer=regularizers.l2())
         self._output = Conv2DTranspose(3, 1, 1, use_bias=True, activation='sigmoid')
 
         self._latent.build(input_shape=input_shape)
@@ -81,35 +82,28 @@ class Generator(tf.keras.Model):
     @staticmethod
     def _compute_latent(x, training):
         mean, log_var = x
-        if training:
-            batch = K.shape(mean)[0]
-            dim = K.int_shape(mean)[1]
-            epsilon = K.random_normal(shape=(batch, dim))
-        else:
-            epsilon = 1.0
+        batch = K.shape(mean)[0]
+        dim = K.int_shape(mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
         return mean + K.exp(log_var / 2) * epsilon
 
     def call(self, inputs, training=None, mask=None):
-        inputs = self._norm(inputs)
         if training:
-            inputs = self._noise(inputs)
+            inputs = self._noise_1(inputs)
+
         inputs = self._conv_1(inputs)
-        inputs = self._conv_2(inputs)
         inputs = self._pool_1(inputs)
         if training:
-            inputs = self._noise(inputs)
+            inputs = self._noise_2(inputs)
         inputs = self._conv_3(inputs)
-        inputs = self._conv_4(inputs)
         inputs = self._pool_2(inputs)
         if training:
-            inputs = self._noise(inputs)
+            inputs = self._noise_3(inputs)
         inputs = self._conv_5(inputs)
-        inputs = self._conv_6(inputs)
         inputs = self._pool_3(inputs)
         if training:
-            inputs = self._noise(inputs)
+            inputs = self._noise_4(inputs)
         inputs = self._conv_7(inputs)
-        inputs = self._conv_8(inputs)
         inputs = self._pool_4(inputs)
 
         inputs = self._flatten(inputs)
@@ -125,14 +119,13 @@ class Generator(tf.keras.Model):
 
         inputs = self._reshape(inputs)  # 1x1xlatent
         inputs = self._generate_1(inputs)  # 2x2x512
-        inputs = self._generate_2(inputs)  # 4x4x256
-        inputs = self._generate_3(inputs)  # 20x20x128
-        inputs = self._generate_4(inputs)  # 20x20x96
-        inputs = self._generate_5(inputs)  # 40x40x72
-        inputs = self._generate_6(inputs)  # 40x40x64
-        inputs = self._generate_7(inputs)  # 80x80x48
-        inputs = self._generate_8(inputs)  # 80x80x48
-        return self._output(inputs)  # 80x80x3
+        inputs = self._generate_2(inputs)  # 4x10x256
+        inputs = self._generate_3(inputs)  # 8x20x128
+        inputs = self._generate_4(inputs)  # 8x20x96
+        inputs = self._generate_5(inputs)  # 16x40x72
+        inputs = self._generate_6(inputs)  # 16x40x64
+        inputs = self._generate_7(inputs)  # 32x80x48
+        return self._output(inputs)  # 32x80x3
 
     def loss(self, actual, predicted):
         reconstruction_loss = tf.reduce_mean(
@@ -171,14 +164,14 @@ def accuracy(y_true, y_pred):
 
 
 if __name__ == '__main__':
-    latent_size = 794
+    latent_size = 1024
     model = Generator(latent_size=latent_size)
     model.build(input_shape=(None, 80, 80, 3))
     model.compile(optimizer=NoisyNadam(strength=.55, sustain=.9, learning_rate=5e-5, beta_1=0.82, beta_2=0.9),
                   loss=model.loss, metrics=[accuracy, 'mae'])
     model.summary()
     inputs = np.zeros((32, 80, 80, 3), dtype=float)
-    outputs = np.ones((32, 80, 80, 3), dtype=float)
+    outputs = np.ones((32, 32, 32, 3), dtype=float)
     model.fit(x=inputs, y=outputs, epochs=3)
     model.save_weights('/tmp/model')
     reloaded = Generator.load('/tmp/model', input_shape=(None, 80, 80, 3), latent_size=latent_size)
@@ -187,5 +180,5 @@ if __name__ == '__main__':
                      loss=reloaded.loss, metrics=[accuracy, 'mae'])
     reloaded.summary()
     reloaded.fit(x=inputs, y=outputs, epochs=1)
-    y = reloaded.call(inputs, False)
+    y = reloaded(inputs, False)
     print(y.shape)
